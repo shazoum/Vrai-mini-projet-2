ba8266d16939e4c2d78f8a05953cf2f1806d8efd
Jeremy D <33804415+shazoum@users.noreply.github.com>
2017-12-02 10:29:17 +0100

    Add files via upload

---
+++ VELO/src/main/java/ch/epfl/cs107/play/game/actor/crate/Crate.java
@@ -0,0 +1,40 @@
+package ch.epfl.cs107.play.game.actor.crate;
+
+import ch.epfl.cs107.play.game.actor.Actor;
+import ch.epfl.cs107.play.game.actor.ActorGame;
+import ch.epfl.cs107.play.game.actor.GameEntity;
+import ch.epfl.cs107.play.math.Transform;
+import ch.epfl.cs107.play.math.Vector;
+import ch.epfl.cs107.play.window.Canvas;
+
+public class Crate extends GameEntity implements Actor {
+	public Crate(ActorGame game, boolean fixed, Vector position) {
+		super(game, fixed, position);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Transform getTransform() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector getVelocity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void draw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean equals(Actor otherActor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
---
+++ VELO/src/main/java/ch/epfl/cs107/play/game/actor/crate/CrateGame.java
@@ -0,0 +1,7 @@
+package ch.epfl.cs107.play.game.actor.crate;
+
+import ch.epfl.cs107.play.game.actor.ActorGame;
+
+public class CrateGame extends ActorGame {
+	
+}
